7b20948eaff40eb59a5764cc1cd7075864398adc
dom_linux <dom_linux@ddddd.xy>
2018-02-03 13:40:28 +0100

    przyklad wywolwania

--- src/main/java/rt/aplikacja/MicroserviceBootSpringIntegrationApplication.java
+++ src/main/java/rt/aplikacja/MicroserviceBootSpringIntegrationApplication.java
@@ -6,6 +6,17 @@ import org.springframework.context.annotation.ImportResource;
 
 
 
+/**
+ * http://localhost:8080/customer
+ * {
+  "id" : "1",
+   "name" : "dupa",
+   "age" : "21"
+}
+ * @author rafal
+ *
+ */
+
 @SpringBootApplication
 @ImportResource({"classpath:spring-integration.xml"})
 public class MicroserviceBootSpringIntegrationApplication {
